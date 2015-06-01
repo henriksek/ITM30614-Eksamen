ad73a3f9636269e54176b1472c6b5f79f5f7ebd1
Henrik Sekkelsten <henrise@hiof.no>
2015-06-01 20:27:12 +0200

    Added pagination to the message-listing + adjusted the footer

--- mysite/theme/views.py
+++ mysite/theme/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from message_app.models import Message
 
@@ -6,5 +7,23 @@ from message_app.models import Message
 
 def frontpage(request):
     messages = Message.objects.all()
-    context = {'messages': messages}
+    
+    page_nr = request.GET.get('page')
+    paginator = Paginator(messages, 10)
+    
+    try:
+        messages = paginator.page(page_nr)
+    
+    except PageNotAnInteger:
+        # If the page is not an integer, go to first page
+        messages = paginator.page(1)
+    
+    except EmptyPage:
+        # If the page is bigger then the number of pages, go to the last page with items on it
+        messages = paginator.page(paginator.num_pages)
+    
+    context = {
+        'messages': messages,
+        'paginator': paginator,
+    }
     return render(request, 'theme/frontpage.html', context)
